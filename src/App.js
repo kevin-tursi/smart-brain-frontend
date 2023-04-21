dd91910b0aab2568e76459b416d9b7896d916620
Kevin Tursi <kevintursi@Kevins-MacBook-Air.local>
2023-04-21 17:00:31 -0400

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 window.process = {};
 
+// test
+
 const returnClarifaiRequestOptions = (imageUrl) => {
   const PAT = 'ce4a9c2002fd410683c444b2f815c5e5';
   const USER_ID = 'kevintursi';
